a0bceb4856dbdb68291e0f77ad22eb0fecd40679
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-05 18:33:02 +0000

    Testing add, subtract, multiply

--- src/tests/Calculator.test.js
+++ src/tests/Calculator.test.js
@@ -1,3 +1,5 @@
+// Unit tests
+
 import React from 'react';
 import Calculator from '../containers/Calculator';
 import {mount, configure} from 'enzyme';
@@ -7,15 +9,74 @@ configure({adapter: new Adapter()});
 
 describe('Calculator', () => {
   let container;
+  let button1;
+  let button2;
+  let button3;
+  let button4;
+  let button5;
+  let button6;
+  let button7;
+  let button8;
+  let button9;
+  let operator_divide;
+  let operator_multiply;
+  let operator_subtract;
+  let operator_add;
+  let operator_equals;
+  let decimal;
+
   beforeEach(() => {
     container = mount(<Calculator/>)
-  })
+    button1 = container.find('#number1');
+    button2 = container.find('#number2');
+    button3 = container.find('#number3');
+    button4 = container.find('#number4');
+    button5 = container.find('#number5');
+    button6 = container.find('#number6');
+    button7 = container.find('#number7');
+    button8 = container.find('#number8');
+    button9 = container.find('#number9');
+    operator_divide = container.find('#operator-divide');
+    operator_multiply = container.find('#operator-multiply');
+    operator_subtract = container.find('#operator-subtract');
+    operator_add = container.find('#operator_add');
+    operator_equals = container.find('#operator-equals');
+    decimal = container.find('#decimal');
+  });
 
   it('should change running total on number enter', () => {
     const button4 = container.find('#number4');
     const runningTotal = container.find('#running-total');
     button4.simulate('click');
     expect(runningTotal.text()).toEqual('4');
-  })
-})
+  });
+
+  it('should add 1 to 4 and get 5', () => {
+    button1.simulate('click');          // Press number1
+    operator_add.simulate('click');     // Press plus
+    button4.simulate('click');          // Press number4
+    operator_equals.simulate('click');  // Press equal
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('5');
+  });
+
+  it('should subtract 4 from 7 and get 3', () => {
+    button7.simulate('click');
+    operator_subtract.simulate('click');
+    button4.simulate('click');
+    operator_equals.simulate('click');
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('3')
+  });
+
+  it('should multiply 3 by 5 and get 15', () => {
+    button3.simulate('click');
+    operator_multiply.simulate('click');
+    button5.simulate('click');
+    operator_equals.simulate('click');
+    const runningTotal = container.find('#running-total');
+    expect(runningTotal.text()).toEqual('15');
+  });
+
+});
 
