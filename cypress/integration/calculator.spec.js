db1c245261618527b00d7939ed47a30a8989269d
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-05 19:57:45 +0000

    UI Integration Tests - chain multiple operations together

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -16,4 +16,15 @@ describe("Calculator", () => {
     cy.get('.display').should('contain', '27');
   });
 
+  it('should chain multiple operations together (90-8) / 2', () => {
+    cy.get('#number9').click();
+    cy.get('#number0').click();
+    cy.get('#operator-subtract').click();
+    cy.get('#number8').click();
+    cy.get('#operator-divide').click();
+    cy.get('#number2').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '41');
+  });
+
 });
