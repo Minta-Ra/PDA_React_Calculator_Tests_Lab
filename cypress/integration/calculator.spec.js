8cfc527628eaaf6a8f9db03d6fff8c0e4bb86b23
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-05 19:46:21 +0000

    Add testing chain multiple operations; Finished Unit Testing

--- cypress/integration/calculator.spec.js
+++ cypress/integration/calculator.spec.js
@@ -1,10 +1,19 @@
 describe("Calculator", () => {
   beforeEach(() => {
     cy.visit("http://localhost:3000");
-  })
+  });
 
   it('should have working number buttons', () => {
     cy.get('#number2').click();
     cy.get('.display').should('contain', '2')
-  })
-})
+  });
+
+  it('should arithmetical operations update the display with the result of the operation', () => {
+    cy.get('#number3').click();
+    cy.get('#operator-multiply').click();
+    cy.get('#number9').click();
+    cy.get('#operator-equals').click();
+    cy.get('.display').should('contain', '27');
+  });
+
+});
