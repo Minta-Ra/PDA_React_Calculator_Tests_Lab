ea41cb3671b3775a101558a0abcdf23227570172
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-07 20:23:57 +0000

    Finished Unit Testing

--- src/containers/Calculator.js
+++ src/containers/Calculator.js
@@ -81,7 +81,11 @@ function App() {
   }
 
   const divide = (number) => {
-    setRunningTotal(parseFloat(previousTotal) / parseFloat(number));
+    if(number == 0) {
+      setRunningTotal("Invalid calculation")
+    } else {
+      setRunningTotal(parseFloat(previousTotal) / parseFloat(number));
+    }
   }
 
 
